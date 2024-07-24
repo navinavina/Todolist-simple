4c103124f78dd62b73c270f2eaf1ffadba106cfb
navinavina <110670317+navinavina@users.noreply.github.com>
2024-07-24 15:03:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const todoList =[{ name : 'make dinner',
+    dueDate: '24-07-2024'},
+    { name :'wash dishes',
+    dueDate: '24-07-24'}];//empty array
+renderTodoList();
+function renderTodoList(){
+
+let todoListHTML ='';
+for (let i =0;i < todoList.length;i++){
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const {name, dueDate} = todoObject;
+
+    const html = `<div>${name}</div>
+    <div> ${dueDate} </div>
+    <button onclick="
+    todoList.splice(${i}, 1);
+    renderTodoList();
+    " class="delete-todo-button">Delete</button>`;
+    todoListHTML += html;
+}
+
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHTML;
+}
+function addTodo(){
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+    todoList.push({
+        name : name,
+        dueDate : dueDate});//adding a value to array
+    
+
+    inputElement.value='';//reset the text in textbox
+    renderTodoList();
+}
